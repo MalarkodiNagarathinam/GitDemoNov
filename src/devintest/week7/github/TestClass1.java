825853f3939f879b8e6a8312f1a2d40613f781ae
malar <malarchandar@gmail.com>
2020-12-01 12:27:11 +0000

    adding 2 classes

---
+++ src/devintest/week7/github/TestClass1.java
@@ -0,0 +1,9 @@
+package devintest.week7.github;
+
+public class TestClass1 {
+
+    public void doSomething() {
+        System.out.println("Hai");
+    }
+
+}
---
+++ src/devintest/week7/github/TestClass2.java
@@ -0,0 +1,9 @@
+package devintest.week7.github;
+
+public class TestClass2 {
+
+
+    public void doSomething() {
+        System.out.println("Hai");
+    }
+}
